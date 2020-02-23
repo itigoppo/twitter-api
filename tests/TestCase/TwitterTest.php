acac29c8ab421e9af48eb6ce579ab5b1b7d1fc0d
itigoppo <itigoppo+github@gmail.com>
2020-02-23 15:53:35 +0900

    first commit

---
+++ tests/TestCase/TwitterTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Itigoppo\Twitter\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class TwitterTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testCreate()
+    {
+        // 空テスト
+        $this->assertTrue(true);
+    }
+}
